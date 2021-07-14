8af1528c81261ab1260233a9bef7879174f02635
devil-cyber <mani2474695@gmail.com>
2021-07-14 07:51:33 +0530

    added route to api and exception handler to api

--- Flash/api.py
+++ Flash/api.py
@@ -17,4 +17,26 @@ class Flash:
 
         # Cached request session
         self._session = None
+    @property
+    def debug(self):
+        return self._debug
+    def route(self,pattern,methods=None):
+        """Decorater that add new route"""
+        def wrapper(handler):
+            self.add_route(pattern,handler,methods)
+            return handler
+        return wrapper
+    def add_route(self,pattern,handler,methods=None):
+        """Add new route"""
+        assert pattern not in self._routes
+        self._routes[pattern] = Route(path_pattern=pattern,handler=handler,methods = methods)
+    def add_exception_handler(self, handler):
+        self._exception_handler = handler
+    def _handle_exception(self, request,response,exception):
+        if self._exception_handler is not None:
+            self._exception_handler(request,response,exception)
+        else:
+            if self._debug is False:
+                raise exception
+
 
